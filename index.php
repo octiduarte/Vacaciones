744789df213ec81eedc987ccab0a3a76d4eebb38
Octavio Duarte <octiduarte@hotmail.com>
2023-11-26 23:46:19 -0300

    first

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Asap:wght@100&family=Inconsolata:wght@300&display=swap"
+        rel="stylesheet">
+
+
+    <title>Vacaciones</title>
+</head>
+
+<body>
+    <div>
+        <div class="burger-icon" onclick="toggleMenu()">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+
+        <div class="menu">
+            <div class="menu-izquierda">
+                <a href="#">Inicio</a>
+            </div>
+            <div class="menu-derecha">
+                <a href="">Registrarse</a>
+                <a href="">Iniciar Sesion</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="javascript/script.js"></script>
+</body>
+
+</html>
