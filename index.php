2a06ef0058e101cee56bb1a9eeae2862dfca863c
octiduarte <octiduarte@hotmail.com>
2023-11-28 12:21:43 -0300

    registro sin terminar

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+<?php
+session_start();
+include('includes/conexion.php');
+conectar();
+?>
 <html lang="en">
 
 <head>
@@ -7,32 +11,43 @@
     <link rel="stylesheet" href="estilos/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Asap:wght@100&family=Inconsolata:wght@300&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Mono:wght@300&display=swap" rel="stylesheet">
 
 
     <title>Vacaciones</title>
 </head>
 
 <body>
-    <div>
-        <div class="burger-icon" onclick="toggleMenu()">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
-        </div>
-
-        <div class="menu">
-            <div class="menu-izquierda">
-                <a href="#">Inicio</a>
+    <header>
+        <div>
+            <div class="burger-icon" onclick="toggleMenu()">
+                <div class="line"></div>
+                <div class="line"></div>
+                <div class="line"></div>
             </div>
-            <div class="menu-derecha">
-                <a href="">Registrarse</a>
-                <a href="">Iniciar Sesion</a>
+
+            <div class="menu">
+                <div class="menu-izquierda">
+                    <a href="#">Inicio</a>
+                </div>
+                <div class="menu-derecha">
+                    <a href="index.php?modulo=registro">Registrarse</a>
+                    <a href="">Iniciar Sesion</a>
+                </div>
             </div>
         </div>
-    </div>
+    </header>
+    <main>
+        <?php
 
+        if (!empty($_GET['modulo'])) {
+            include('modulos/' . $_GET['modulo'] . '.php');
+        } else {
+            ?>
+            <?php
+        }
+        ?>
+    </main>
     <script src="javascript/script.js"></script>
 </body>
 
---
+++ modulos/registro.php
@@ -0,0 +1,32 @@
+<div class="contenedor_registro">
+
+
+    <div class="div_registro">
+        <div>
+            <h1 style="font-weight:normal;" >Registro</h1>
+            <hr>
+        </div>
+        <form action="">
+            <div class="div_interno_form">
+                <h3>Nombre</h3>
+                <input type="text" name="nombre" id="nombre">
+            </div>
+            <div class="div_interno_form">
+                <h3>Apellido</h3>
+                <input type="text" name="apellido" id="apellido" placeholder="Apellido">
+            </div>
+            <div class="div_interno_form">
+                <h3>DNI</h3>
+                <input type="number" name="dni" id="dni">
+            </div>
+            <div class="div_interno_form">
+                <h3>Correo Electronico</h3>
+                <input type="text" name="correo" id="correo">
+            </div>
+            <div class="div_interno_form">
+                <h3>Contraseña</h3>
+                <input type="password" name="clave" id="clave">
+            </div>
+        </form>
+    </div>
+</div>
